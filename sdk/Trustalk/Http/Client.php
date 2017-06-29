a5f11e688c517e0992095638fa1ae9c0b568a6ab
ym.teruya <ym.teruya@lexues.co.jp>
2017-06-29 19:05:30 +0900

    アカウントクラス実装中

---
+++ sdk/Trustalk/Http/Client.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Trustalk\Http;
+
+interface Client
+{
+    public function request($method, $url, $params = array(), $data = array(),
+                            $headers = array(), $user = null, $password = null,
+                            $timeout = null);
+}
--- sdk/Trustalk/Rest/Client.php
+++ sdk/Trustalk/Rest/Client.php
@@ -2,14 +2,25 @@
 
 namespace Trustalk\Rest;
 
+use Trustalk\Http\CurlClient;
+use Trustalk\VersionInfo;
+use Trustalk\Rest\Api\Account\AccountList as Account;
+
 class Client
 {
+    protected $username;
+    protected $password;
+
+    public function __construct($username = null, $password = null)
+    {
+    }
+
     static function sample()
     {
         $array1 = array("testA" => 1, "testB" => 2);
         $array2 = array("testC" => 3, "testD" => 4);
         $test = $array1 + $array2;
-        // var_dump($test);
+        var_dump("doyaaa");
         return $test;
     }
 }
